b52efa9755eb12ed6592f01188f04edc256f2ebe
WAHU0508 <sharongikenye@gmail.com>
2024-06-27 17:41:15 +0300

    Add arrow functions

---
+++ index.js
@@ -0,0 +1,40 @@
+//test1: Divide Function
+/*function divide1(a, b) {
+    return a / b;
+}
+console.log(divide1(2000, 100));
+const divide2 = (a, b) => a / b;
+
+console.log(divide2(2000, 100));
+*/
+const divide = function (a, b) {
+    return a / b;
+}
+console.log(divide(2000, 100));
+
+
+//test2: Square Fuction
+/* function square1(a) {
+    return a ** 2;
+}
+console.log(square1(5));
+
+const square2 = function(a) {
+    return a ** 2;
+}
+console.log(square(6));*/
+
+const square = (a) => a ** 2
+console.log(square(7));
+
+//test3: Add Functions
+/* function add1(a, b) {
+    return a + b;
+}
+console.log(add1(3, 4))
+const add2 = function (a, b) {
+    return a + b;
+}
+console.log(add2(5, 6))*/
+const add = (a, b) => a + b
+console.log(add(7, 8))
